e9a63243bad66fb4775eff6724937d345586238e
nkanda7mukiri3 <mukirinkanda@gmail.com>
2023-06-18 01:56:49 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point
+ *
+ * Return - Always 0 indicates success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
