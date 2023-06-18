42e9102063c5ff880373e99b58dfa8e7c02537d8
nkanda7mukiri3 <mukirinkanda@gmail.com>
2023-06-18 01:46:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point
+ *
+ * Return: Always 0 indicates success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
