0875e36c3ca409ed39044bcc98bad6cdca225b4d
nkanda7mukiri3 <mukirinkanda@gmail.com>
2023-06-18 03:41:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(7, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 20);
 	return (1);
 }
