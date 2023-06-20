2903f679a58870b7f417e8488e378b5d59c61bfd
nkanda7mukiri3 <mukirinkanda@gmail.com>
2023-06-19 18:35:40 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point
+ *
+ * Description: Prints all possible combos of single digit num
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(n);
+		if (n == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
