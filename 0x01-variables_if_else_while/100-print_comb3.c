11c824719d9150a42813e3335b8187c409435cdd
nkanda7mukiri3 <mukirinkanda@gmail.com>
2023-06-19 19:13:56 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point
+ *
+ * Description: Prints all possible diff combos of two digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 48; x <= 56; x++)
+	{
+		for (y = 49; y <= 57; y++)
+		{
+			if (y > x)
+			{
+				putchar(x);
+				putchar(y);
+				if (x != 56 || y != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
