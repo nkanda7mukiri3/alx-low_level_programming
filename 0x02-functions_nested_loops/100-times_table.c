da394fab6ef9fcc7bf38a999c5891b364bb5b763
nkanda7mukiri3 <mukirinkanda@gmail.com>
2023-06-21 08:44:54 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,17 +24,19 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(' ');
 					_putchar(k + 48);
 				}
-				else if (k >= 10)
+				else if (k >= 10 && k <= 100)
 				{
 					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + 48);
 					_putchar((k % 10) + 48);
 				}
 			}
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
