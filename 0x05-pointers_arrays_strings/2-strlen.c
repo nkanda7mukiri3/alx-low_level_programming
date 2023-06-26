ceb97dba80fb6cc781f01978df6ef822abdd274d
nkanda7mukiri3 <mukirinkanda@gmail.com>
2023-06-26 14:18:37 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: input string
+ *
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+	}
+
+	return (l);
+}
