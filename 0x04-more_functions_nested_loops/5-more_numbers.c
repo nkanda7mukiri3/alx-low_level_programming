7d87501e2b11c84e7fb07b10cbef1813572704a7
nkanda7mukiri3 <mukirinkanda@gmail.com>
2023-06-26 12:31:27 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,11 @@ void more_numbers(void)
 
 	for (j = 0; j < 10; j++)
 	{
-		for (i = '0'; i <= '14'; i++)
+		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i);
+			if (i >= 10)
+				_putchar('1');
+				_putchar(i % 10);
 		}
 		_putchar('\n');
 	}
