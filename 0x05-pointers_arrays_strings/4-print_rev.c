5fa38e11b4b9f9694c0326609c180807abc01cf7
nkanda7mukiri3 <mukirinkanda@gmail.com>
2023-06-26 15:31:21 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+
+	for (i = l; i >= 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
