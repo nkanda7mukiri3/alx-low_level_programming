54d42691cdc6d91e568f7866be4f430ade86f511
nkanda7mukiri3 <mukirinkanda@gmail.com>
2023-07-04 08:30:11 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * char *_strchr - locates char in string
+ * @s: string input
+ * @c: char to locate
+ *
+ * Return: address of char or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] > '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		else
+			return (0);
+	}
+}
