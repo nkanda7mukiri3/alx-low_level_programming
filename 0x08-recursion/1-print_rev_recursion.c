cc25d6c3991a87ae650ecb58568d5faf55b57d68
nkanda7mukiri3 <mukirinkanda@gmail.com>
2023-07-09 03:44:57 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in rev
+ * @s: string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+
+	else
+	{
+		_putchar('\n');
+	}
+}
