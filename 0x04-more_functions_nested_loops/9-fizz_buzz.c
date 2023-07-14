777a5e85b44e74031129a7b323194a8fe37774dd
nkanda7mukiri3 <mukirinkanda@gmail.com>
2023-07-14 00:37:51 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,25 +14,25 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 3 == 0 && n % 5 != 0)
+		if (n == 1)
 		{
-			printf("Fizz ");
+			printf("%d", n);
+		}
+		else if (n % 3 == 0 && n % 5 != 0)
+		{
+			printf(" Fizz");
 		}
 		else if (n % 5 == 0 && n % 3 != 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if (n % 3 == 0 && n % 5 == 0)
 		{
-			printf("FizzBuzz ");
-		}
-		else if (n == 100)
-		{
-			printf("Buzz");
+			printf(" FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", n);
+			printf(" %d", n);
 		}
 	}
 	printf("\n");
