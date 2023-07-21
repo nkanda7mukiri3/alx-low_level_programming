669d0588ea528958386fe5e16e30928195e87ef2
nkanda7mukiri3 <mukirinkanda@gmail.com>
2023-07-20 17:09:04 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,7 +12,6 @@ void print_all(const char * const format, ...)
 	va_list v;
 
 	va_start(v, format);
-
 	if (format)
 	{
 		while (format[u])
@@ -23,12 +22,13 @@ void print_all(const char * const format, ...)
 				printf("%s%c", t, va_arg(v, int));
 			else if (format[u] == 'f')
 				printf("%s%c", t, va_arg(v, int));
-			else if (format[u] == 's'){
+			else if (format[u] == 's')
+			{
 				s = va_arg(v, char *);
 				if (!s)
 					s = "(nil)";
 				printf("%s%s", t, s);
-			}else
+			} else
 				u++;
 				continue;
 			t = ", ";
