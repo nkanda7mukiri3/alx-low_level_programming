4242667301df1cfb8765c297780ddee2d4572b6e
nkanda7mukiri3 <mukirinkanda@gmail.com>
2023-09-04 18:12:00 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - creates file
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ *
+ * Return: 1(success), -1(failure)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fileD, w, length = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length] != '\0'; length++)
+		{
+		}
+	}
+
+	fileD = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fileD, text_content, length);
+
+	if (fileD == -1 || w == -1)
+	{
+		return (-1);
+	}
+
+	close(fileD);
+
+	return (1);
+}
